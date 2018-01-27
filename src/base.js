e865f4523e142d7fed56c2d48aa0ecac93b9cd52
Chevalier Thomas <thomas.chevalier@hei.yncrea.fr>
2018-01-27 12:54:58 +0100

    node

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyBESfBjAQVOA5Xol6U5seBG2sMITjoHV5E",
+   authDomain: "chatbox-56d4b.firebaseapp.com",
+   databaseURL: "https://chatbox-56d4b.firebaseio.com"
+})
+
+export default base;
---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {render} from 'react-dom';
+import "./index.css";
+import Connexion from "./components/Connexion";
+import App from "./components/App";
+import {BrowserRouter, Match, Miss} from 'react-router';
+import NotFound from "./components/NotFound";
+const Root = ()=>{
+  return(
+    <BrowserRouter>
+      <div>
+        <Match exactly pattern="/" component={Connexion} />
+        <Match pattern="/pseudo/:pseudo" component={App} />
+        <Miss component={NotFound}/>
+      </div>
+    </BrowserRouter>
+  )
+}
+
+
+render(
+  <Root />,
+  document.getElementById('root')
+)
